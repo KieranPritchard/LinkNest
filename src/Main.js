c55076d96f4e35e5bc01ded4f180c8f5fdabf546
Kieran Pritchard <KieranPritchard06@gmail.com>
2024-11-17 14:48:56 +0000

    Fixed spelling error.

--- src/Main.js
+++ src/Main.js
@@ -1,6 +1,6 @@
 function openSocialLink(platform){
     if (platform == 'Github') {
-        console.log("Open Github")        
+        console.log("Open GitHub")        
     } else if (platform == 'Linkedin'){
         console.log("Open Linkedin")
     }
