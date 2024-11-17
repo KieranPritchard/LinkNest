4f2d1b5af50fec88f60bd7918da52c76fe7d3c57
Kieran Pritchard <KieranPritchard06@gmail.com>
2024-11-17 14:43:00 +0000

    Readded function that opens links.

---
+++ src/Main.js
@@ -0,0 +1,7 @@
+function openSocialLink(platform){
+    if (platform == 'Github') {
+        console.log("Open Github")        
+    } else if (platform == 'Linkedin'){
+        console.log("Open Linkedin")
+    }
+}
