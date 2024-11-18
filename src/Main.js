a7422408895c0d56cc561e95862aa975aba67b53
Kieran Pritchard <KieranPritchard06@gmail.com>
2024-11-18 19:14:28 +0000

    Added links to JavaScript.

--- src/Main.js
+++ src/Main.js
@@ -1,7 +1,9 @@
 function openSocialLink(platform){
     if (platform == 'Github') {
+        window.open("https://github.com/KieranPritchard")
         console.log("Open GitHub")        
     } else if (platform == 'Linkedin'){
+        window.open("https://www.linkedin.com/in/kieran-pritchard-20278430b/")
         console.log("Open Linkedin")
     }
 }
