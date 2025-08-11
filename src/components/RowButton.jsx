97d8aba13c779c60088028e3c29ca7105bf8835c
Kieran Pritchard <KieranPritchard06@gmail.com>
2025-08-11 19:22:56 +0100

    fixed button row

--- src/components/RowButton.jsx
+++ src/components/RowButton.jsx
@@ -4,6 +4,16 @@ import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
 import { faMessage } from '@fortawesome/free-solid-svg-icons';
 
 function RowButton(props){
+    const handleClick = (text) => {
+        navigator.clipboard.writeText(text)
+        .then(() => {
+            console.log('Text copied to clipboard!');
+        })
+        .catch(err => {
+            console.error('Failed to copy text: ', err);
+        });
+    }
+
     const handleIcon = () => {
         switch (props.Icon) {
             case "phone":
@@ -21,7 +31,7 @@ function RowButton(props){
     }
     
     return(
-        <button className="bg-[#d7e1ec] rounded-full h-12 w-12">
+        <button onClick={() => handleClick(props.contact)} className="bg-[#d7e1ec] hover:text-[#d7e1ec] hover:bg-transparent border-2 border-[#d7e1ec] rounded-full h-12 w-12">
             <FontAwesomeIcon icon={handleIcon()} />
         </button>
     )
