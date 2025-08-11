90beebb18c27fadd88839bd6962e83d0e30be4ba
Kieran Pritchard <KieranPritchard06@gmail.com>
2025-08-11 19:03:09 +0100

    added links

--- src/App.jsx
+++ src/App.jsx
@@ -6,12 +6,15 @@ import ProfileImage from './components/ProfileImage'
 
 function App() {
   return(
-    <div className='h-screen bg-linear-to-br from-[#0d1b2a] to-[#1b2b41]'>
+    <div className='h-auto bg-linear-to-br from-[#0d1b2a] to-[#1b2b41] py-4'>
       <ProfileImage />
       <NameBox />
       <ButtonRow />
       <div className='mx-32'>
         <LinkButton buttonName={"Linkedin"} buttonLink={"www.linkedin.com/in/kieran-pritchard"} />
+        <LinkButton buttonName={"GitHub"} buttonLink={"https://github.com/KieranPritchard"} />
+        <LinkButton buttonName={"Try Hack Me"} buttonLink={"https://tryhackme.com/p/NightProxy"} />
+        <LinkButton buttonName={"Portfolio"} buttonLink={"https://kieranpritchard.github.io/Kieran-Pritchard-Portfolio/"} />
       </div>
     </div>
   )
