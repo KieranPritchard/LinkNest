a2aedcf780b88245fb0e96331808e0342f34d3f2
Kieran Pritchard <KieranPritchard06@gmail.com>
2025-08-11 20:39:41 +0100

    fixed proportions

--- src/components/LinkButton.jsx
+++ src/components/LinkButton.jsx
@@ -4,7 +4,7 @@ function LinkButton(props){
     }
 
     return(
-        <button className="bg-[#d7e1ec] text-[#0d1b2a] p-4 w-full rounded-xl hover:bg-transparent border-2 border-[#d7e1ec] hover:text-[#d7e1ec] text-lg my-2" onClick={() => handleLink(props.buttonLink)}>
+        <button className="bg-[#d7e1ec] text-[#0d1b2a] h-14 w-full rounded-xl hover:bg-transparent border-2 border-[#d7e1ec] hover:text-[#d7e1ec] text-lg my-2" onClick={() => handleLink(props.buttonLink)}>
             {props.buttonName}
         </button>
     )
--- src/components/ProfileImage.jsx
+++ src/components/ProfileImage.jsx
@@ -3,7 +3,7 @@ import Picture from '../assets/Image-Of-Self-1.webp'
 function ProfileImage(){
     return(
         <div className='flex justify-center pt-4'>
-            <img className='h-64 w-64 rounded-full' src={Picture} alt="Image of me" />
+            <img className='h-85 w-85 rounded-full' src={Picture} alt="Image of me" />
         </div>
     )
 }
