13fa611bfa604ab4d5c75c6d2bd3027cdee08057
Kieran Pritchard <KieranPritchard06@gmail.com>
2025-08-12 12:14:46 +0100

    adjusted shadow

--- src/components/Toast.jsx
+++ src/components/Toast.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect } from "react";
 
 function Toast({ message, duration = 3000, onClose }) {
     useEffect(() => {
@@ -7,7 +7,7 @@ function Toast({ message, duration = 3000, onClose }) {
     }, [duration, onClose]);
 
     return (
-        <div className="fixed top-5 right-5 bg-[#1b2b41] border-4 border-[#a9d6e5] text-[#f9fafb] py-2.5 px-5 rounded-md shadow-lg">
+        <div className="fixed top-5 right-5 bg-[#1b2b41] border-4 border-[#a9d6e5] text-[#f9fafb] py-2.5 px-5 rounded-md shadow-lg shadow-[#a9d6e5]">
         {message}
         </div>
     );
