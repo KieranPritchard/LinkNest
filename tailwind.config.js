4b1e61eb0c51f9feb6c5b24d6eac20e479f0cfca
Kieran Pritchard <KieranPritchard06@gmail.com>
2025-09-02 13:57:31 +0100

    add dark mode compatiblity

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+    darkMode: "class",
     content: [
         "./src/**/*.{js,jsx,ts,tsx}",
     ],
     theme: {
         extend: {
-            colors:{
-                primary: "rgb(var(--background) / <alpha-value>)",
-                secondary: "rgb(var(--background-secondary) / <alpha-value>)",
-                text: "rgb(var(--base-text)) / <alpha-value>)",
-                accent: "rgb(var(--main-accent)) / <alpha-value>)",
-                border: "rgb(var(--border)) / <alpha-value>)",
-                hover: "rgb(var(--hover-highlight)) / <alpha-value>)",
-            },
             fontFamily: {
                 redhat: ['"Red Hat Display"', 'sans-serif'],
                 sourcecode: ['"Source Code Pro"', 'monospace'],
