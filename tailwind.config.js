76a66e61ad815d1437d5909ba7523bdda215e163
Kieran Pritchard <KieranPritchard06@gmail.com>
2025-09-02 13:09:24 +0100

    added root colours

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,15 +5,23 @@ module.exports = {
     ],
     theme: {
         extend: {
-        fontFamily: {
-            redhat: ['"Red Hat Display"', 'sans-serif'],
-            sourcecode: ['"Source Code Pro"', 'monospace'],
-        },
-        fontWeight: {
-            900: '900',
-            400: '400',
-        },
+            colors:{
+                primary: "rgb(var(--background) / <alpha-value>)",
+                secondary: "rgb(var(--background-secondary) / <alpha-value>)",
+                text: "rgb(var(--base-text)) / <alpha-value>)",
+                accent: "rgb(var(--main-accent)) / <alpha-value>)",
+                border: "rgb(var(--border)) / <alpha-value>)",
+                hover: "rgb(var(--hover-highlight)) / <alpha-value>)",
+            },
+            fontFamily: {
+                redhat: ['"Red Hat Display"', 'sans-serif'],
+                sourcecode: ['"Source Code Pro"', 'monospace'],
+            },
+            fontWeight: {
+                900: '900',
+                400: '400',
+            },
         },
     },
     plugins: [],
-    }
+}
