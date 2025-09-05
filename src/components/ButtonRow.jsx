5c75bd56aa0537b63c406f8a00000de85b0aaf38
Kieran Pritchard <KieranPritchard06@gmail.com>
2025-09-05 20:33:06 +0100

    added handlin functions to row button

--- src/components/ButtonRow.jsx
+++ src/components/ButtonRow.jsx
@@ -3,9 +3,9 @@ import RowButton from "./RowButton"
 function ButtonRow(){
     return(
         <div className="flex flex-row justify-center my-4 gap-4">
-            <RowButton icon={"phone"} />
-            <RowButton icon={"email"} />
-            <RowButton icon={"message"} />
+            <RowButton icon={"phone"} details={"+44 07763 534145"}/>
+            <RowButton icon={"email"} details={"KieranPritchard06@gmail.com"}/>
+            <RowButton icon={"message"} details={"+44 07763 534145"}/>
         </div>
     )
 }
