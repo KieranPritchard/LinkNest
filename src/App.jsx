e1aaffb2ce64f015ecb309570c73c9b233c4589b
Kieran Pritchard <KieranPritchard06@gmail.com>
2026-01-30 21:44:25 +0000

    fixed my whitespace issues

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,12 @@
 import './App.css'
-import ButtonRow from './components/ButtonRow'
-import DownloadCvButton from './components/buttons/DownloadCvButton'
-import Footer from './components/main_elements/Footer'
-import LinkButton from './components/LinkButton'
-import NameBox from './components/about/NameBox'
-import ProfileImage from './components/about/ProfileImage'
-import ShareButton from './components/ShareButton'
 
 function App() {
   return(
-    <div className='min-h-screen bg-gradient-to-br dark:from-[#0d1b2a] dark:to-[#1b2b41] from-[#f9fafb] to-[#d7e1ec] py-4 px-[5%]'>
-      <ProfileImage />
-      <NameBox />
-      <ButtonRow />
-      <div className="lg:w-3/4 lg:mx-auto">
-        <DownloadCvButton /> 
-        <LinkButton buttonName={"Linkedin"} buttonLink={"https://www.linkedin.com/in/kieran-pritchard/"} />
-        <LinkButton buttonName={"GitHub"} buttonLink={"https://github.com/KieranPritchard"} />
-        <LinkButton buttonName={"Try Hack Me"} buttonLink={"https://tryhackme.com/p/NightProxy"} />
-        <LinkButton buttonName={"Portfolio"} buttonLink={"https://kieranpritchard.github.io/"} />
-        <ShareButton /> 
+    <>
+      <div className='min-h-screen bg-gradient-to-br dark:bg-[#0d1b2a] bg-[#f9fafb] py-4'>
+      
       </div>
-      <Footer />
-    </div>
+    </>
   )
 }
 
