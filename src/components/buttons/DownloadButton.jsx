453b59cfa5ee63eb3645afd8abd1e711289dce4b
Kieran Pritchard <KieranPritchard06@gmail.com>
2026-02-01 16:12:50 +0000

    refactored component

--- src/components/buttons/DownloadButton.jsx
+++ src/components/buttons/DownloadButton.jsx
@@ -1,13 +1,12 @@
 import { faFile } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import Cv from '../assets/Kieran_Pritchard_C.V.pdf'
 
-function DownloadCvButton(){
+function DownloadButton({file, fileName, text}){
     return(
         <a
             className="relative flex items-center justify-between bg-[#2a3c4b] dark:bg-[#a9d6e5] text-[#f9fafb] dark:text-[#0d1b2a] border-[#2a3c4b] dark:border-[#a9d6e5] hover:text-[#2a3c4b] dark:hover:text-[#a9d6e5] h-14 w-full rounded-xl border-2 text-lg my-2 hover:bg-transparent transition-colors duration-300 ease-in-out px-4"
-            href={Cv}
-            download={"Kieran_Pritchard_C.V.pdf"}
+            href={file}
+            download={fileName}
         >
             {/* Icon on the far left */}
             <span>
@@ -16,10 +15,10 @@ function DownloadCvButton(){
 
             {/* Text centered */}
             <span className="absolute left-1/2 transform -translate-x-1/2">
-                Download My C.V
+                {text}
             </span>
         </a>
     )
 }
 
-export default DownloadCvButton
+export default DownloadButton
