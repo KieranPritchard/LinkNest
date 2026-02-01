59d0216a999511403aa5f7e0a04a273b391e1237
Kieran Pritchard <KieranPritchard06@gmail.com>
2026-02-01 16:19:41 +0000

    reformatted buttons

--- src/components/buttons/ShareButton.jsx
+++ src/components/buttons/ShareButton.jsx
@@ -1,9 +1,11 @@
 import { useState } from "react";
-import qrCode from "../assets/qr_code.webp";
 
-function ShareButton() {
+// Button which features a image drop down
+function DropDownButton({text, image, altText}) {
+    // State to tract wheter the button is open or not
     const [isOpen, setIsOpen] = useState(false);
 
+    // Function to handle clice and set the drop down as open or closed
     const handleClick = () => setIsOpen(prev => !prev);
 
     return (
@@ -11,25 +13,27 @@ function ShareButton() {
             onClick={handleClick}
             className={`flex flex-col items-center justify-center bg-[#2a3c4b] dark:bg-[#a9d6e5] text-[#f9fafb] dark:text-[#0d1b2a] border-[#2a3c4b] dark:border-[#a9d6e5] hover:text-[#2a3c4b] dark:hover:text-[#a9d6e5] w-full rounded-xl border-2 text-lg my-2 hover:bg-transparent transition-all duration-300 ease-in-out px-4 ${isOpen ? "py-5" : "h-14"} `}
         >
-        <p className="transition-all duration-300 ease-in-out">Share with others</p>
+        {/* uses text props */}
+        <p className="transition-all duration-300 ease-in-out">{text}</p>
 
-            {/* QR code container */}
+            {/* Image container */}
             <div
                 className={`
                 w-full overflow-hidden transition-all duration-300 ease-in-out
                 ${isOpen ? "max-h-[500px] opacity-100 pt-5" : "max-h-0 opacity-0 pt-0"}
                 `}
             >
+                {/* Displays the image inside a flex comtainer */}
                 <div className="flex justify-center">
-                <img
-                    src={qrCode}
-                    alt="QR Code"
-                    className="aspect-square h-75 sm:h-95 pb-5 transition-all duration-300"
-                />
+                    <img
+                        src={image}
+                        alt={altText}
+                        className="aspect-square h-75 sm:h-95 pb-5 transition-all duration-300"
+                    />
                 </div>
             </div>
         </div>
     );
 }
 
-export default ShareButton;
+export default DropDownButton;
