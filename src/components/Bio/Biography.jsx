8efc01ebf0799bbb7508d03d6bfb970b43ea0ca3
Kieran Pritchard <KieranPritchard06@gmail.com>
2026-02-04 18:31:06 +0000

    added new text

--- src/components/Bio/Biography.jsx
+++ src/components/Bio/Biography.jsx
@@ -8,9 +8,10 @@ function Biography(){
                 <h3 className="text-2xl font-bold mb-2">Bio:</h3>
                 {/* Fixed: Adjusted text size for mobile and added better color contrast for light mode */}
                 <p className="text-base md:text-lg leading-relaxed text-[#1B2B41] dark:text-[#f9fafb]">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-                    Nisi error nostrum cum, ipsa numquam dolores mollitia eligendi, 
-                    quaerat provident excepturi rerum impedit consequatur.
+                    T Level Software Development student at BPC and aspiring ethical hacker. 
+                    Driven by a hands-on approach to cybersecurity, 
+                    I thrive on tackling technical challenges and building security 
+                    solutions through direct project experience.
                 </p>
             </div>
 
